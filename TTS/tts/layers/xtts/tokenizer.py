d58f960a1982f4398eeb5d06b93218f543edfa4d
Igor Degterov <igor@degterov@epam.com>
2024-11-13 21:29:30 +0100

    Update dependencies to generic, increased limit

--- TTS/tts/layers/xtts/tokenizer.py
+++ TTS/tts/layers/xtts/tokenizer.py
@@ -621,7 +621,7 @@ class VoiceBpeTokenizer:
 
     def check_input_length(self, txt, lang):
         lang = lang.split("-")[0]  # remove the region
-        limit = self.char_limits.get(lang, 300)
+        limit = self.char_limits.get(lang, 2000)
         if len(txt) > limit:
             print(
                 f"[!] Warning: The text length exceeds the character limit of {limit} for language '{lang}', this might cause truncated audio."
